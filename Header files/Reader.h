2a0799e8b3bc289aaf1b8097b51e2dfd17d0f330
MinniE <63558665+minnib578@users.noreply.github.com>
2020-12-14 13:56:30 -0500

    Add files via upload

--- Header files/Reader.h
+++ Header files/Reader.h
@@ -1,1 +1,18 @@
+#include "GL\glew.h"
+#include "GL\freeglut.h"
+#include <vector>
+#include <string>
+using namespace std;
+class Reader {
+public:
+	Reader(string filename);
+	void Draw_POINT();
+	void Draw_LINE();
+	void Draw_TRIANGLE();
+	float bounding;
+private:
+	vector<vector<GLfloat>>Vertexs;
+	vector<vector<GLint>>Face;
+	
+};
 
